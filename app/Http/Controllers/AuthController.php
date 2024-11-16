2725571a54108efd0c1f51cf380b09be93c918e2
moosbeere_O <moosbeere_O@mail.ru>
2024-11-16 10:44:24 +0300

    Lab 3

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,11 +6,21 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    protected $redirect = '/';
     public function signup(){
         return view('auth.signup');
     }
 
     public function registr(Request $request){
-        
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required|min:6'
+        ]);
+        $response=[
+            'name'=>$request->name,
+            'email'=>request('email'),
+        ];
+        return response()->json($response);
     }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -43,17 +43,16 @@
           </ul>
         </li> -->
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+       <a class="btn btn-outline-success" href="/auth/signup" type="submit">SignUp</a>
+   
     </div>
   </div>
 </nav>
 </header>
 <main>
-    <p> Hello, World!</p>
+  <div class="container mt-3">
     @yield('content')
+  </div>
 </main>
-    </body>
+</body>
 </html>
