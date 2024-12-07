404f2e4ecaa529c5a36a7e8ce3d0a82a75e66eef
moosbeere_O <moosbeere_O@mail.ru>
2024-12-07 11:13:13 +0300

    Lab10

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use App\Events\NewArticleEvent;
 
 
 class ArticleController extends Controller
@@ -44,8 +45,10 @@ class ArticleController extends Controller
         $article->name = $request->name;
         $article->desc = $request->desc;
         $article->user_id = 1;
-        $article->save();
-        return redirect('/article');
+        if ($article->save()) {
+            NewArticleEvent::dispatch($article);
+            return redirect('/article');
+        }
     }
 
     /**
